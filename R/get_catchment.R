fcad53e53b20c69df88f6c2d5e22ad309083809a
deankoch <deankoch@gmail.com>
2023-06-15 14:32:47 -0700

    finished docs for get_catchment

--- R/get_catchment.R
+++ R/get_catchment.R
@@ -1,37 +1,53 @@
-#' Return a list of geometry objects describing watershed upstream
+#' Return a list of NHD geometry objects modelling the catchment for an outlet
 #'
-#' This uses the `nhdR` package to manage downloads of NHD data collections
-#' related to the watershed upstream of the point `outlet`.  If `outlet` is
-#' a character string instead, it is passed to `nominatim_point` to get the point
-#' from OSM.
+#' This uses the `nhdR` package to manage ETL of NHD data collections related to
+#' the catchment of the point `outlet`.  If `outlet` is a character string, it is
+#' passed to `nominatim_point` to get the point from OSM.
 #' 
-#' The function downloads three collections (see `?get_upstream`) from NHD
-#' containing highly detailed information on sub-catchments and stream reaches,
-#' which is opened and processed to determine the catchment for `outlet`.
+#' The function downloads three collections from NHD (see `?get_upstream`) containing
+#' geometries and attributes on stream networks, (sub)catchments, and water bodies
+#' upstream of `outlet`. It returns the relevant objects in a list, including a
+#' boundary polygon representing the full catchment.
+#' 
+#' Users who are only interested in this boundary polygon can speed things somewhat by
+#' setting `fast=TRUE`. This returns the boundary along with the outlet point and its
+#' COMID, and omits the more detailed data model for the stream network and sub-catchments. 
 #' 
 #' Note that `nhdR` uses `rappdirs` to manage persistent storage. This will set
-#' up a default data storage folder on your system and use cached files instead
-#' of downloading repeatedly whenever possible.
+#' up a default data storage folder on your system and, whenever possible, use cached
+#' files instead of downloading things repeatedly.
+#' 
+#' `crs_out` can be set to NA to return everything in its original projection.
+#' Otherwise the function transforms all geo-referenced output data to the supplied
+#' CRS. If `crs_out=NULL` (the default), the function assigns the UTM projection for
+#' the zone overlying the `outlet`, 
 #' 
 #' The function returns a list with the following
 #' 
-#' * `outlet`
-#' * `flow`
-#' * `catchment`
-#' * `lake`
-#' * `edge`
-#' * `boundary`
-#' * `comid`
-#'
+#' * `comid` integer COMID associated with the outlet 
+#' * `outlet` sfc_POINT, the outlet location
+#' * `boundary` sfc_POLYGON, the catchment boundary inscribing all areas draining to `outlet`
+#' 
+#' If `fast=FALSE`, the returned list also includes
+#' 
+#' * `edge` data frame of edges in the directed graph representing stream flow in the NHD model
+#' * `catchment` sf data frame with polygon geometries, the upstream (sub)catchments  
+#' * `flow` sf data frame with line geometries, the upstream stream reaches (possibly artificial)
+#' * `lake` sf data frame with polygon geometries, the upstream water bodies
+#' 
 #' All geo-referenced outputs are converted to the common projection `crs_out`. If this
 #' is `NULL` (the default) the function uses the UTM zone overlying `outlet`.
+#' 
+#' See https://cran.r-project.org/package=nhdplusTools and the references pages linked
+#' there for more on the NHDPlus data model, and how the COMID ties things together.
 #'
 #' @param outlet character to query OSM for location or else a point accepted by `sf::st_geometry`
 #' @param crs_out output CRS, anything acceptable to `sf::st_crs` or NULL to use UTM zone of outlet
+#' @param fast logical, if TRUE the `catchment` object is not returned, speeding things somewhat
 #'
 #' @return list
 #' @export
-get_catchment = function(outlet, crs_out=NULL) {
+get_catchment = function(outlet, crs_out=NULL, fast=FALSE) {
   
   # look up locations for character input and/or convert to WGS84 to match NHD data
   if( is.character(outlet) ) outlet = nominatim_point(outlet)
@@ -53,60 +69,78 @@ get_catchment = function(outlet, crs_out=NULL) {
   
   ## fetch data
   
-  # download the NHDPlus data for this VPU
-  nhdR::nhd_plus_get(vpu=uid, 'NHDSnapshot', temporary=FALSE)
+  # download required NHD data for the VPU
   nhdR::nhd_plus_get(vpu=uid, 'NHDPlusAttributes', temporary=FALSE)
   nhdR::nhd_plus_get(vpu=uid, 'NHDPlusCatchment', temporary=FALSE)
   
-  # load relevant datasets into R
-  message('loading NHD stream network geometries')
-  flow_line = nhdR::nhd_plus_load(vpu=uid, 'NHDSnapshot', 'NHDFlowline', quiet=TRUE)
-  
+  # open directed edge map 
   message('loading NHD stream network edges')
-  flow_df = nhdR::nhd_plus_load(vpu=uid, 'NHDPlusAttributes', 'PlusFlow', quiet=TRUE)
+  edge = nhdR::nhd_plus_load(vpu=uid, 'NHDPlusAttributes', 'PlusFlow', quiet=TRUE)
   
-  message('loading NHD lake polygons')
-  lake_poly = nhdR::nhd_plus_load(vpu=uid, 'NHDSnapshot', 'NHDWaterbody', quiet=TRUE)
-  
+  # open catchment polygons (slow)
   message('loading NHD catchment polygons')
   catch_poly = nhdR::nhd_plus_load(vpu=uid, 'NHDPlusCatchment', 'Catchment', quiet=TRUE)
   
-  # find upstream components
-  message('\n')
-  result_list = get_upstream(outlet, flow_line, catchment=catch_poly, lake=lake_poly, edge=flow_df)
+  # simpler sub-routine when only the catchment boundary is needed
+  if(fast) {
+    
+    # find upstream components
+    message('')
+    result_list = get_upstream(outlet, edge, catchment=catch_poly, fast=TRUE)
+    
+  } else {
   
-  # set up output CRS
-  message('\ntransforming results to output projection') 
+    # download the NHDSnapshot data to get flow lines and lakes in the VPU
+    nhdR::nhd_plus_get(vpu=uid, 'NHDSnapshot', temporary=FALSE)
+    
+    # open flow lines (slow)
+    message('loading NHD stream network geometries')
+    flow_line = nhdR::nhd_plus_load(vpu=uid, 'NHDSnapshot', 'NHDFlowline', quiet=TRUE)
+    
+    # open lake polygons
+    message('loading NHD lake polygons')
+    lake_poly = nhdR::nhd_plus_load(vpu=uid, 'NHDSnapshot', 'NHDWaterbody', quiet=TRUE)
+    
+    # find upstream components
+    message('')
+    result_list = get_upstream(outlet, edge, catchment=catch_poly, flow=flow_line, lake=lake_poly)
+  }
+  
+  # set up output names and projection
   if( is.null(crs_out) ) crs_out = epsg_utm 
-  nm_transform = c('outlet', 'flow', 'catchment', 'lake', 'boundary')
-  result_list[nm_transform] = result_list[nm_transform] |> lapply(\(x) sf::st_transform(x, crs_out))
-  
-  # check for dangling stream reaches
-  is_inside = result_list[['flow']] |> sf::st_intersects(result_list[['boundary']], sparse=FALSE) 
-  result_list[['flow']] = result_list[['flow']][is_inside,]
+  if( !anyNA(crs_out) ) {
+    
+    nm_transform = c('outlet', 'flow', 'catchment', 'lake', 'boundary')
+    nm_transform = nm_transform[ nm_transform %in% names(result_list) ]
+    message('\ntransforming results to output projection') 
+    result_list[nm_transform] = result_list[nm_transform] |> lapply(\(x) sf::st_transform(x, crs_out))
+  }
+
   return(result_list)
 }
 
 
 #' Return a list of NHD geometry objects corresponding to the catchment for an outlet
 #' 
-#' This returns a list of flow lines and sub-catchments within the catchment for the
+#' This returns a list of geometries and other information describing the catchment for the
 #' supplied outlet point `outlet`. It first identifies the element of `catchment` containing
 #' the outlet and exhaustively traces all upstream paths by following the directed paths in
 #' `edge` and collecting the corresponding elements of `catchment` and `flow`.
 #' 
-#' Arguments `flow`, `catchment`, `lake` and `edge` should all be created using calls to
-#' `nhdR::nhd_plus_load` with arguments "dsn" and "component" set appropriately (see code
-#' in `get_upstream` for example).
-#' 
 #' The function returns the relevant subsets of these four datasets, along with a copy
 #' of `outlet`, the COMID for outlet, and a new polygon representing the boundary of
-#' the entire catchment.
+#' the entire catchment. 
+#' 
+#' Arguments `edge`, `catchment`, `flow`, and `lake` should all be created using calls to
+#' `nhdR::nhd_plus_load` with arguments "dsn" and "component" set appropriately (see code
+#' in `get_upstream` for example). Arguments `flow` and/or `lake` can be `NULL`, in which
+#' case the corresponding output objects are omitted. Set `fast=TRUE` to omit all four,
+#' and only return the outlet, COMID, and boundary.
 #' 
 #' Having encountered duplicate vertex issues in NHD data while using the S2 Geometry
-#' Library - terra's default - we now include the `s2` argument to allow switching this
-#' off (temporarily) so that the GeographicLib library is used instead for intersection
-#' calculations.
+#' Library (terra's default) we now include the `s2` argument to allow switching this
+#' off temporarily during the function call. This causes terra to switch to the higher-
+#' precision GeographicLib library for the problematic intersection calculations.
 #'
 #' @param outlet an sfc_POINT object locating the outlet
 #' @param flow the "NHDFlowline" dataset from the "NHDSnapshot" component
@@ -114,10 +148,11 @@ get_catchment = function(outlet, crs_out=NULL) {
 #' @param lake the "NHDWaterbody" dataset from NHD from the "NHDSnapshot" component
 #' @param edge data frame, the "PlusFlow" dataset from the "NHDPlusAttributes" component
 #' @param s2 logical if `FALSE` the function sets `sf::sf_use_s2(FALSE)` for computations
+#' @param fast logical if `TRUE` only the outlet, COMID, and boundary computed and returned
 #'
 #' @return list containing appropriate subsets of flow, catchment, lake, edge (see details) 
 #' @export
-get_upstream = function(outlet, flow, catchment, lake, edge, s2=FALSE) {
+get_upstream = function(outlet, edge, catchment, flow=NULL, lake=NULL, s2=FALSE, fast=FALSE) {
   
   # follows the directed edges in `edge` to find the components of `flow` and `catchment`
   # that drain into `outlet`. These are returned in a list of geometries and data frames,
@@ -136,7 +171,7 @@ get_upstream = function(outlet, flow, catchment, lake, edge, s2=FALSE) {
   outlet_on_catch = sf::st_transform(outlet, catch_crs)
   
   # find the (sub)catchment COMID for our outlet point
-  message('identifying outlet sub-catchment')
+  message('locating outlet')
   covers_catch = catchment |> 
     sf::st_geometry() |> 
     sf::st_intersects(outlet_on_catch, sparse=FALSE) |> 
@@ -171,11 +206,6 @@ get_upstream = function(outlet, flow, catchment, lake, edge, s2=FALSE) {
   # vector of all COMID codes found upstream of outlet
   sws_comid = comid_caught[comid_caught != 0]
   sws_poly = catchment[catchment[['FEATUREID']] %in% sws_comid,] |> sf::st_make_valid()
-  
-  # find subset of relevant edge data and flow-lines
-  is_sws_edge = edge[['TOCOMID']] %in% sws_comid
-  is_sws_flow = flow[['COMID']] %in% sws_comid
-  message('found ', paste(sum(is_sws_flow), 'stream reach(es)'))
 
   # boundary polygon from union of associated sub-catchments - computations can be slow
   message('merging ', length(sws_comid), ' sub-catchment polygons')
@@ -192,16 +222,38 @@ get_upstream = function(outlet, flow, catchment, lake, edge, s2=FALSE) {
     sf::st_sfc(crs=catch_crs) |> 
     suppressMessages()
   
-  # find lakes
-  is_sws_lake = sf::st_intersects(lake, boundary, sparse=FALSE) |> suppressMessages()
-  message('found ', sum(is_sws_lake), ' lake(s)')
+  # initialize output list
+  out_list = list(comid = outlet_comid,
+                  outlet = outlet,
+                  boundary = boundary)
   
-  # copy relevant subsets to output list
-  return(list(outlet = outlet,
-              flow = flow[which(is_sws_flow)[!is_dangling],],
-              catchment = sws_poly,
-              lake = lake[is_sws_lake,],
-              edge = edge[is_sws_edge,],
-              boundary = boundary,
-              comid = outlet_comid))
+  if(fast) return( out_list )
+  
+  # copy edges and catchments subsets
+  is_sws_edge = edge[['TOCOMID']] %in% sws_comid
+  out_list[['edge']] = edge[is_sws_edge,]
+  out_list[['catchment']] = sws_poly
+  
+  # copy flow line subset
+  if( !is.null(flow) ) {
+    
+    # find subset of relevant flow-lines
+    is_sws_flow = flow[['COMID']] %in% sws_comid
+    message('found ', paste(sum(is_sws_flow), 'stream reach(es)'))
+    flow_out = flow[is_sws_flow,]
+    
+    # check for dangling stream reaches
+    is_inside = sf::st_intersects(flow_out, boundary, sparse=FALSE) |> suppressMessages()
+    out_list[['flow']] = flow_out[is_inside,]
+  }
+  
+  # copy lakes subset
+  if( !is.null(lake) ) {
+    
+    is_sws_lake = sf::st_intersects(lake, boundary, sparse=FALSE) |> suppressMessages()
+    message('found ', sum(is_sws_lake), ' lake(s)')
+    out_list[['lake']] = lake[is_sws_lake,]
+  }
+
+  return(out_list)
 }
