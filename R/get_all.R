760e16bf3d94fb9938d077122f208f2ed01ec070
deankoch <deankoch@gmail.com>
2023-06-26 21:24:40 -0700

    added splitter to the end of the build script

--- R/get_all.R
+++ R/get_all.R
@@ -19,6 +19,7 @@ fetch_all = function(data_dir, outlet=NULL, overwrite=FALSE, force_overwrite=FAL
   land_path = data_dir |> save_land()
   soils_path = data_dir |> save_soils()
   nwis_path = data_dir |> save_nwis(nwis_nm)
+  split_path = data_dir |> save_split()
   
   # concatenate paths in list and return from list mode
   all_path = list(nhd=nhd_path, ned=dem_path, soils=soils_path)
@@ -28,6 +29,8 @@ fetch_all = function(data_dir, outlet=NULL, overwrite=FALSE, force_overwrite=FAL
   if( force_overwrite | !all(file.exists(nhd_path)) ) {
     
     # NHD data: for UYR initial download is slow, but after that can run in < 1 min
+    message('')
+    message('fetching catchment model...')
     catch_list = get_catch(outlet)
     save_catch(data_dir, catch_list, overwrite=TRUE)
   }
@@ -36,6 +39,8 @@ fetch_all = function(data_dir, outlet=NULL, overwrite=FALSE, force_overwrite=FAL
   if( force_overwrite | !all(file.exists(dem_path)) ) {
     
     # NED data download for UYR completes in < 1 min
+    message('')
+    message('fetching DEM...')
     dem = get_dem(data_dir)
     save_dem(data_dir, dem, overwrite=TRUE)
   }
@@ -44,13 +49,17 @@ fetch_all = function(data_dir, outlet=NULL, overwrite=FALSE, force_overwrite=FAL
   if( force_overwrite | !all( file.exists( unlist(nwis_path) ) ) ) {
     
     # small batch of downloads, should complete in < 5 min  
+    message('')
+    message('fetching stream gages...')
     get_nwis(data_dir, nwis_nm, from_initial=nwis_from)
   }
   
   # get land use data from GAP/LANDFIRE
   if( force_overwrite | !all( file.exists( unlist(land_path) ) ) ) {
     
-    # several GB to download here, after initial download runs in < 2 min  
+    # several GB to download here, after initial download runs in < 2 min
+    message('')
+    message('fetching land use...')
     land = get_land(data_dir)
     save_land(data_dir, land, overwrite=TRUE)
   }
@@ -59,7 +68,19 @@ fetch_all = function(data_dir, outlet=NULL, overwrite=FALSE, force_overwrite=FAL
   if( force_overwrite | !all( file.exists( unlist(soils_path) ) ) ) {
     
     # several GB to download here, after initial download runs in < 2 min  
-    soils = get_soils(data_dir)
-    save_soils(data_dir, soils, overwrite=TRUE)
+    message('')
+    message('fetching soils...')
+    soil = get_soils(data_dir)
+    save_soils(data_dir, soil, overwrite=TRUE)
+  }
+  
+  # split into sub-catchments and copy relevant data subsets
+  sub_exists = length(split_path[['sub']]) > 0
+  if( force_overwrite | !sub_exists ) {
+    
+    message('')
+    message('splitting at gages...')
+    sub_list = get_split(data_dir)
+    save_split(data_dir, sub_list, overwrite=TRUE,  nwis_nm=nwis_nm)
   }
 }
