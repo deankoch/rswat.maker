ce691466aae7a44fa17d46fcdb727298207fb52e
deankoch <deankoch@gmail.com>
2023-07-13 20:15:34 -0700

    new scratch pad for vignette draft stuff

--- dev_testing.R
+++ dev_testing.R
@@ -6,7 +6,7 @@ data_dir = 'D:/rswat_data/yellowstone'
 
 
 # full update
-# outlet = nominatim_point("Carter's Bridge, MT")
-# outlet |> run_rqswat(data_dir, overwrite=TRUE, no_download=TRUE)
+outlet = nominatim_point("Carter's Bridge, MT")
+outlet |> run_rqswat(data_dir, nwis_from=as.Date('2005-01-01'), overwrite=TRUE, no_download=TRUE)
 
 # outlet |> run_qswat(data_dir)
