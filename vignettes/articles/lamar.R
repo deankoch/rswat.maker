e20051ff96d1ea41aeae9b4da916c8c79988974f
deankoch <deankoch@gmail.com>
2023-07-27 12:28:08 -0700

    this script runs the setup in the Lamar example ahead then renders the article

---
+++ vignettes/articles/lamar.R
@@ -0,0 +1,56 @@
+# Helper script for the lamar.Rmd article
+
+library(rswat.maker)
+
+# At the end of the script we build the article
+
+# markdown directory
+markdown_file = 'D:/rswat.maker/vignettes/articles/lamar.Rmd'
+
+# local path to outputs folders
+data_dir_uyrw = 'D:/rswat_data/yellowstone'
+data_dir_smaller = 'D:/rswat_data/lamar_contemporary'
+maker_path = data_dir_smaller |> file.path('run_maker_log.txt')
+
+# This runs the Lamar river QSWAT+ setup example initially to establish files
+# and get a complete log. The "lamar.Rmd" article uses snippets from the output
+# in this log file (copy/pasted by hand) rather than generating it each time
+# the article is rendered (or the cache is replaced).
+
+# Lamar river gage site coordinates (in WGS84)
+outlet = c(-110.400953708362735, 44.928980820264847) |> 
+  sf::st_point() |> 
+  sf::st_sfc(crs=4326)
+
+# two sinks to save console output from the smaller example
+if( !dir.exists(data_dir_smaller) ) dir.create(data_dir_smaller)
+maker_file = maker_path |> file(open = 'wt')
+maker_file |> sink(type='output')
+maker_file |> sink(type='message')
+
+# run the entire workflow on smaller example
+data_dir_smaller |> run_maker(what = NULL,
+                              outlet = outlet,
+                              nwis_from = as.Date('2005-01-01'),
+                              overwrite = TRUE,
+                              no_download = TRUE)
+
+# close the logfile
+sink(type='output')
+sink(type='message')
+
+# Upper Yellowstone River outlet at Carter's Bridge
+outlet_uyrw = c(-110.5665168, 45.5969578) |> 
+  sf::st_point() |> 
+  sf::st_sfc(crs=4326)
+
+# now run the workflow on Yellowstone, up to splitting, all gage years
+data_dir_uyrw |> run_maker(what=c('catch', 'dem', 'nwis', 'land' , 'soil', 'split'),
+                           outlet = outlet_uyrw,
+                           nwis_from = NULL,
+                           overwrite = TRUE,
+                           no_download = TRUE)
+                               
+# when finished, render the article
+rmarkdown::render(markdown_file)
+
